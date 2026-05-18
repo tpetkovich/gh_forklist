c00e1b5f83be64bd984d15fdb24d031cfaf8d710
Tamas Petkovich <petkow@gmail.com>
2026-05-18 17:04:21 +0200

    fix for the compare API call (direction), indent correction

--- gh_forklist.py
+++ gh_forklist.py
@@ -8,7 +8,6 @@
 # ]
 # [tool.uv]
 # exclude-newer = "2025-08-01T00:00:00Z"
-#
 # ///
 
 import sys
@@ -18,147 +17,164 @@ import json
 import pandas as pd
 from datetime import datetime
 
-
 base_args = [
-    "gh",
-    "api",
-    "--method",
-    "GET",
-    "-H",
-    "Accept: application/vnd.github+json",
-    "-H",
-    "X-GitHub-Api-Version: 2026-03-10",
+   "gh",
+   "api",
+   "--method",
+   "GET",
+   "-H",
+   "Accept: application/vnd.github+json",
+   "-H",
+   "X-GitHub-Api-Version: 2026-03-10",
 ]
 
 
 def gh_cli_api(endpoint_args, exit_on_api_error: bool = False):
 
-    result = None
+   result = None
 
-    try:
-        result = subprocess.run(
-            base_args + endpoint_args,
-            encoding="utf-8",
-            capture_output=True,
-            text=True,
-            check=True,
-        )
-    except subprocess.CalledProcessError as e:
-        print(f"Error output: {e.stderr}")
-        if exit_on_api_error:
-            sys.exit(1)
-    except FileNotFoundError:
-        print("The command executable was not found")
-        sys.exit(1)
+   try:
+      result = subprocess.run(
+         base_args + endpoint_args,
+         encoding="utf-8",
+         capture_output=True,
+         text=True,
+         check=True,
+      )
+   except subprocess.CalledProcessError as e:
+      print(f"Error output: {e.stderr}")
+      if exit_on_api_error:
+         sys.exit(1)
+   except FileNotFoundError:
+      print("The command executable was not found")
+      sys.exit(1)
 
-    return result
+   return result
 
 
 def generate_forklist(repo: str, branch: str, include_identical: bool):
 
-    repo_ls = repo.split("/")
+   repo_ls = repo.split("/")
 
-    repo_endpoint = f"repos/{repo_ls[0]}/{repo_ls[1]}"
-    forks_endpoint = repo_endpoint + "/forks"
+   repo_endpoint = f"repos/{repo_ls[0]}/{repo_ls[1]}"
+   forks_endpoint = repo_endpoint + "/forks"
 
-    if branch is None:
-        default_branch_result = gh_cli_api([repo_endpoint], exit_on_api_error=True)
-        branch = json.loads(default_branch_result.stdout)["default_branch"]
+   if branch is None:
+      default_branch_result = gh_cli_api([repo_endpoint], exit_on_api_error=True)
+      branch = json.loads(default_branch_result.stdout)["default_branch"]
 
-    forks_result = gh_cli_api(
-        ["--paginate", "-f", "sort=stargazers", forks_endpoint], exit_on_api_error=True
-    )
+   forks_result = gh_cli_api(
+      ["--paginate", "-f", "sort=stargazers", forks_endpoint], exit_on_api_error=True
+   )
 
-    fork_fields_1 = [
-        "full_name",
-        "default_branch",
-        "stargazers_count",
-        "forks_count",
-        "open_issues_count",
-        "size",
-        "created_at",
-        "updated_at",
-        "pushed_at",
-    ]
+   assert forks_result is not None
 
-    df = pd.json_normalize(json.loads(forks_result.stdout), max_level=0)[fork_fields_1]
+   fork_fields_1 = [
+      "full_name",
+      "default_branch",
+      "stargazers_count",
+      "forks_count",
+      "open_issues_count",
+      "size",
+      "created_at",
+      "updated_at",
+      "pushed_at",
+   ]
 
-    df[["created_at", "updated_at", "pushed_at"]] = df[
-        ["created_at", "updated_at", "pushed_at"]
-    ].apply(pd.to_datetime)
+   df = pd.json_normalize(json.loads(forks_result.stdout), max_level=0)[fork_fields_1]
 
-    def get_fork_details(fork_repo: str, upstream_owner: str, branch: str):
+   df[["created_at", "updated_at", "pushed_at"]] = df[
+      ["created_at", "updated_at", "pushed_at"]
+   ].apply(pd.to_datetime)
 
-        comp_endpoint = f"/repos/{fork_repo}/compare/master...{upstream_owner}:{branch}"
+   def get_fork_details(
+      fork_repo: str, fork_branch: str, upstream_repo: list[str], upstream_branch: str
+   ):
 
-        details_result = gh_cli_api(["-f", "per_page=1", comp_endpoint])
+      comp_endpoint = f"/repos/{fork_repo}/compare/{upstream_repo[0]}:{upstream_repo[1]}:{upstream_branch}...{fork_branch}"
 
-        try:
-            response = json.loads(details_result.stdout)
-            out = {
-                "status": response["status"],
-                "ahead_by": response["ahead_by"],
-                "behind_by": response["behind_by"],
-            }
-        except Exception as e:
-            out = {"status": pd.NA, "ahead_by": pd.NA, "behind_by": pd.NA}
-            print("Error", e)
+      details_result = gh_cli_api(["-f", "per_page=1", comp_endpoint])
 
-        print(fork_repo, ":", repr(out))
+      try:
+         response = json.loads(details_result.stdout)
+         out = {
+               "status": response["status"],
+               "ahead_by": response["ahead_by"],
+               "behind_by": response["behind_by"],
+               "total_commits": response["total_commits"],
+         }
+      except Exception as e:
+         out = {
+               "status": pd.NA,
+               "ahead_by": pd.NA,
+               "behind_by": pd.NA,
+               "total_commits": pd.NA,
+         }
+         print("Error", e)
 
-        return pd.Series(out)
+      print(fork_repo, ":", repr(out))
 
-    df_status = df["full_name"].apply(
-        func=get_fork_details, upstream_owner=repo_ls[0], branch=branch
-    )
+      return pd.Series(out)
 
-    df = df.join(df_status)
+   df_status = df.apply(
+      lambda x: get_fork_details(
+         fork_repo=x.full_name,
+         fork_branch=x.default_branch,
+         upstream_repo=repo_ls,
+         upstream_branch=branch
+      ),
+      axis=1
+   )
 
-    filename = (
-        "forks_" + repo_ls[1] + "_" + datetime.now().strftime("%Y%m%d%H%M%S") + ".csv"
-    )
+   df = df.join(df_status)
 
-    if include_identical:
-        df.to_csv(filename, index=False)
-    else:
-        df[df.status != "identical"].to_csv(filename, index=False)
+   filename = (
+      "forks_" + repo_ls[1] + "_" + datetime.now().strftime("%Y%m%d%H%M%S") + ".csv"
+   )
+
+   if include_identical:
+      df.to_csv(filename, index=False)
+   else:
+      df[df.status != "identical"].to_csv(filename, index=False)
 
 
 def build_parser() -> argparse.ArgumentParser:
 
-    PROG_NAME = "gh_forklist"
+   PROG_NAME = "gh_forklist"
 
-    p = argparse.ArgumentParser(
-        prog=PROG_NAME,
-        description="Simple Github CLI wrapper to list the forks of a target Github repository with commit divergence details saved to a .csv file. Requires Github CLI executable `gh` in path with authentication set up.",
-    )
+   p = argparse.ArgumentParser(
+      prog=PROG_NAME,
+      description="Simple Github CLI wrapper to list the forks of a target Github repository with commit divergence details saved to a .csv file. Requires Github CLI executable `gh` in path with authentication set up.",
+   )
 
-    p.add_argument("repo", metavar="REPO", help="The target Github repository in owner/repo format")
+   p.add_argument(
+      "repo", metavar="REPO", help="The target Github repository in owner/repo format"
+   )
 
-    p.add_argument(
-        "-b",
-        metavar="BRANCH",
-        help="The relevant branch of the target repository. If unspecified, uses the default one.",
-    )
+   p.add_argument(
+      "-b",
+      metavar="BRANCH",
+      help="The relevant branch of the target repository. If unspecified, uses the default one.",
+   )
 
-    p.add_argument(
-        "--include-identical",
-        action="store_true",
-        help="Include identical/unchanged forks in the list, which are by default discarded.",
-    )
+   p.add_argument(
+      "--include-identical",
+      action="store_true",
+      help="Include identical/unchanged forks in the list, which are by default discarded.",
+   )
 
-    return p
+   return p
 
 
 def main(argv=None) -> int:
 
-    parser = build_parser()
-    args = parser.parse_args(argv)
+   parser = build_parser()
+   args = parser.parse_args(argv)
 
-    generate_forklist(args.repo, args.b, args.include_identical)
+   generate_forklist(args.repo, args.b, args.include_identical)
 
-    return 0
+   return 0
 
 
 if __name__ == "__main__":
-    sys.exit(main())
+   sys.exit(main())
